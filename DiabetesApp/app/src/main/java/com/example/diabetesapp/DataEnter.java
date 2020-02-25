1e69225c3c4408c6faa500c3da11beca72aca04e
Ebin Benny <ebinmoolan@gmail.com>
2020-02-25 21:57:24 +0000

    Call backend from patient app

--- DiabetesApp/app/src/main/java/com/example/diabetesapp/DataEnter.java
+++ DiabetesApp/app/src/main/java/com/example/diabetesapp/DataEnter.java
@@ -1,21 +1,28 @@
 package com.example.diabetesapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.graphics.Bitmap;
-import java.io.FileOutputStream;
 
-import android.provider.MediaStore;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.diabetesapp.data.requests.StoreBSLRequest;
+import com.example.diabetesapp.data.requests.StoreRBPRequest;
+import com.example.diabetesapp.data.requests.StoreWeightRequest;
+import com.example.diabetesapp.data.responses.StoreBSLResponse;
+import com.example.diabetesapp.data.responses.StoreRBPResponse;
+import com.example.diabetesapp.data.responses.StoreWeightResponse;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileOutputStream;
+import java.sql.Timestamp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,25 +32,16 @@ public class DataEnter extends AppCompatActivity {
     static final int REQUEST_PRESSURE = 1;
     static final int REQUEST_IMAGE_CAPTURE = 2;
 
-    Button back;
-    Button enter;
+    Bitmap bitmap;
 
-    TextView sugar;
-    TextView pressure;
+    Button back, enter;
 
-    ImageView sugar_picture;
-    ImageView sugar_numbers;
+    TextView sugar, pressure;
+    EditText height, weight;
 
-    ImageView pressure_picture;
-    ImageView pressure_numbers;
+    String sugar_data, pressure_data;
 
-    EditText height;
-    EditText weight;
-
-    String sugar_data;
-    String pressure_data;
-
-    public static Bitmap bitmap;
+    int mPatientID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +62,12 @@ public class DataEnter extends AppCompatActivity {
             }
         });
 
-        sugar_picture = findViewById(R.id.camera);
-        sugar_numbers = findViewById(R.id.numbers);
-        pressure_picture = findViewById(R.id.camera2);
-        pressure_numbers = findViewById(R.id.numbers2);
+        ImageView sugar_picture = findViewById(R.id.camera);
+        ImageView sugar_numbers = findViewById(R.id.numbers);
+        ImageView pressure_picture = findViewById(R.id.camera2);
+        ImageView pressure_numbers = findViewById(R.id.numbers2);
+
+        mPatientID = getIntent().getIntExtra("tag", -1);
 
         sugar_picture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +83,9 @@ public class DataEnter extends AppCompatActivity {
         });
         pressure_picture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { takePhoto("pressure", 1); }
+            public void onClick(View view) {
+                takePhoto("pressure", 1);
+            }
         });
         pressure_numbers.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,8 +119,8 @@ public class DataEnter extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-            if (resultCode == DataEnter.RESULT_OK) {
-                switch (requestCode) {
+        if (resultCode == DataEnter.RESULT_OK) {
+            switch (requestCode) {
                 case (0): {
                     sugar_data = data.getStringExtra("sugar");
                     if (sugar_data != null) sugar.setText(sugar_data);
@@ -130,22 +132,22 @@ public class DataEnter extends AppCompatActivity {
                 case (REQUEST_IMAGE_CAPTURE): {
                     String type = data.getStringExtra("type");
                     int code = data.getIntExtra("code", 1);
-                        Bundle extras = data.getExtras();
-                        Bitmap imageBitmap = (Bitmap) extras.get("data");
-                        bitmap = imageBitmap;
-                        File photoFile = new File(this.getFilesDir(), "Image.jpg");
-                        try {
-                            FileOutputStream out = new FileOutputStream(photoFile);
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-                            out.flush();
-                            out.close();
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    bitmap = imageBitmap;
+                    File photoFile = new File(this.getFilesDir(), "Image.jpg");
+                    try {
+                        FileOutputStream out = new FileOutputStream(photoFile);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                        out.flush();
+                        out.close();
 
-                            Intent intent = new Intent(this, Camera.class);
-                            intent.putExtra("type", type);
-                            startActivityForResult(intent, code);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        Intent intent = new Intent(this, Camera.class);
+                        intent.putExtra("type", type);
+                        startActivityForResult(intent, code);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             }
@@ -153,10 +155,56 @@ public class DataEnter extends AppCompatActivity {
     }
 
     private void enterData() {
-        String h = height.getText().toString();
-        String w = weight.getText().toString();
-        String sugar = sugar_data;
-        String pressure = pressure_data;
+        String timestamp = new Timestamp(System.currentTimeMillis()).toString();
+        try {
+            StoreWeightRequest storeWeightRequest = new StoreWeightRequest(mPatientID, timestamp, Float.parseFloat(weight.getText().toString()));
+            storeWeightRequest.makeRequest(this, new Response.Listener<StoreWeightResponse>() {
+                @Override
+                public void onResponse(StoreWeightResponse response) {
+                    Log.println(Log.INFO, "StoreWeightRequest", response.success.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.println(Log.ERROR, "StoreWeightRequest", error.getMessage());
+                }
+            });
+        } catch (Exception e) {
+
+        }
+        try {
+            StoreBSLRequest storeBSLRequest = new StoreBSLRequest(mPatientID, timestamp, Float.parseFloat(sugar_data), null);
+            storeBSLRequest.makeRequest(this, new Response.Listener<StoreBSLResponse>() {
+                @Override
+                public void onResponse(StoreBSLResponse response) {
+                    Log.println(Log.INFO, "StoreBSLRequest", response.success.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.println(Log.ERROR, "StoreBSLRequest", error.getMessage());
+                }
+            });
+        } catch (Exception e) {
+
+        }
+        try {
+            StoreRBPRequest storeRBPRequest = new StoreRBPRequest(mPatientID, timestamp, Float.parseFloat(pressure_data), Float.parseFloat(pressure_data));
+            storeRBPRequest.makeRequest(this, new Response.Listener<StoreRBPResponse>() {
+                @Override
+                public void onResponse(StoreRBPResponse response) {
+                    Log.println(Log.INFO, "StoreRBPRequest", response.success.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.println(Log.ERROR, "StoreRBPRequest", error.getMessage());
+
+                }
+            });
+        } catch (Exception e) {
+
+        }
     }
 
     private void back() {
